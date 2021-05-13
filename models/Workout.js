4b85b76f3ae6428f56d0b23b89ed3e6047268e0f
markeezy <markkhoo95@gmail.com>
2021-05-12 23:54:24 -0700

    init some base files

---
+++ models/Workout.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+
+});
+
+const User = mongoose.model("User", orkoutSchema);
+
+module.exports = User;
