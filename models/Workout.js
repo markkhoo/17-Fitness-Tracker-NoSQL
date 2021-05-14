7cae1f17cbb5046750e20049644b6c4e3835ce1c
markeezy <markkhoo95@gmail.com>
2021-05-13 21:11:10 -0700

    complete model

--- models/Workout.js
+++ models/Workout.js
@@ -3,9 +3,40 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                required: "This type is required"
+            },
+            name: {
+                type: String,
+                required: "This name is required"
+            },
+            duration: {
+                type: Number,
+                required: true
+            },
+            distance: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            }
+        }
+    ]
 });
 
-const User = mongoose.model("User", orkoutSchema);
+const User = mongoose.model("User", WorkoutSchema);
 
 module.exports = User;
